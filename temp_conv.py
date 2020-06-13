e6a1569e1a38fdd99644040e3f00be7876482ced
Tuhin Kumar Dutta <tuhindutta79@gmail.com>
2020-06-13 20:42:43 +0530

    temp_conv.py modified (Kelvin scale added)

--- temp_conv.py
+++ temp_conv.py
@@ -1,15 +1,26 @@
 #Temperature conversion
 
 import math
-tempunit=input('Enter temperature unit type, (C)elcius / (F)arenheit:')
+tempunit=input('Enter temperature unit type, (C)elcius / (F)arenheit/ (K)elvin:')
 temp=float(input('Enter temperature: '))
 
 if tempunit.lower() == 'c':
     conv = float((9*(temp/5))+32)
+    conv2 = float(temp + 273.15)
     print(f'Temperature in Farenheit: {conv}°F')
-    
+    print(f'Temperature in Kelvin: {conv2}°K')
+
 elif tempunit.lower() == 'f':
     conv = float(((temp-32)*5)/9)
+    conv2 = float(conv + 273.15)
     print(f'Temperature in Celcius: {conv}°C')
+    print(f'Temperature in Kelvin: {conv2}°K')
+
+elif tempunit.lower() == 'k':
+    conv = float(temp - 273.15)
+    conv2 = float((9*(conv/5))+32)
+    print(f'Temperature in Celcius: {conv}°C')
+    print(f'Temperature in Farenheit: {conv2}°F')
+
 else:
     print('Invalid Input')
