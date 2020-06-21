b7ec8764f571af66d5688882ba2db319abfc3bcf
Tuhin Kumar Dutta <tuhindutta79@gmail.com>
2020-06-21 20:34:48 +0530

    square_matrix_sudoku modified

--- square_matrix_sudoku/sudoku.py
+++ square_matrix_sudoku/sudoku.py
@@ -7,21 +7,22 @@ import pandas as pd
 obj = pd.read_csv ('sudoku.csv')
 arr = obj.to_numpy()
 
-dim = np.arange(arr.shape[0])
+rows = np.arange(arr.shape[1])
+columns = np.arange(arr.shape[0])
 
 print(arr)
 
 result = []
 
-for shape in dim:
-    if np.count_nonzero(arr[:,shape] == arr[shape, shape]) > 1 or np.count_nonzero(arr[shape, :] == arr[shape, shape]) > 1:
-        result.append(0)
-    else:
-        result.append(1)
+for row_item in rows:
+    for column_item in columns:
+        if np.count_nonzero(arr[:,column_item] == arr[row_item, column_item]) > 1 or np.count_nonzero(arr[row_item, :] == arr[row_item, column_item]) > 1:
+            result.append(0)
+        else:
+            result.append(1)
         
         
 if result.count(0) > 0:
-
     print('You lost. Try again')
 else:
     print('You won')
