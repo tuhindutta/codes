08c3ce04de3d70f7eb2adcd4eecc8a643b7ffeb0
Tuhin Kumar Dutta <tuhindutta79@gmail.com>
2020-06-22 08:48:29 +0530

    sudoku & hangman.py updated

--- hangman.py
+++ hangman.py
@@ -1,34 +1,38 @@
-word = list('python')
-guessed = list(len(word) * '_')
+def hangman(word, max_attempts):
 
-max_attempts = int(input('No. of attempts you want: '))
-attempts = list(range(max_attempts))
-attempts_done = 0
-appends = 0
-for attempt in attempts:
-    while attempts_done != max_attempts:
-        while '_' in guessed:            
-            letter = input('Enter your guessed letter: ')
-            if letter in word:
-                if letter in guessed:                    
-                    print('You already guessed the letter')
-                else:                   
-                    guessed[word.index(letter)] = letter
-                    appends += 1
-                    print(f'Your guessed letter {letter} is right in {guessed}')
-                left = guessed.count('_')              
-                print(f'You have {left} letters to guess within {max_attempts - attempts_done} wrong attempts')
-        
+    word = list(word)
+    guessed = list(len(word) * '_')
+
+    #max_attempts = int(input('No. of attempts you want: '))
+    attempts = list(range(max_attempts))
+    attempts_done = 0
+    appends = 0
+    for attempt in attempts:
+        while attempts_done != max_attempts:
+            while '_' in guessed:            
+                letter = input('Enter your guessed letter: ')
+                if letter in word:
+                    if letter in guessed:                    
+                        print('You already guessed the letter')
+                    else:                   
+                        guessed[word.index(letter)] = letter
+                        appends += 1
+                        print(f'Your guessed letter {letter} is right in {guessed}')
+                    left = guessed.count('_')              
+                    print(f'You have {left} letters to guess within {max_attempts - attempts_done} wrong attempts')
+
+                else:
+                    attempts_done += 1
+                    left = guessed.count('_')
+                    print(f'Your guess is wrong. You have {max_attempts - attempts_done} attempts left & {left} letters to guess in {guessed}') 
+                    break
+
             else:
-                attempts_done += 1
-                left = guessed.count('_')
-                print(f'Your guess is wrong. You have {max_attempts - attempts_done} attempts left & {left} letters to guess in {guessed}') 
+                print ('You won')
                 break
-        
-        else:
-            print ('You won')
-            break
 
-    else:
-        print('You lost')
-        break
+        else:
+            print('You lost')
+            break
+            
+hangman('python', 4)
---
+++ sudoku/sudoku.py
@@ -0,0 +1,30 @@
+def sudoku(path):
+
+    import numpy as np
+    import pandas as pd
+
+    obj = pd.read_csv (path)
+    arr = obj.to_numpy()
+
+    rows = np.arange(arr.shape[1])
+    columns = np.arange(arr.shape[0])
+
+    print(arr)
+
+    result = []
+
+    for row_item in rows:
+        for column_item in columns:
+            if np.count_nonzero(arr[:,column_item] == arr[row_item, column_item]) > 1 or np.count_nonzero(arr[row_item, :] == arr[row_item, column_item]) > 1:
+                result.append(0)
+            else:
+                result.append(1)
+
+
+    if result.count(0) > 0:
+        print('You lost. Try again')
+    else:
+        print('You won')
+
+        
+sudoku('sudoku.csv')
