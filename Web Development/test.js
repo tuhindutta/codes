028d8ab41fada3ad7c2d15b6e0015528ad6dd26c
Tuhin Kumar Dutta <tuhindutta79@gmail.com>
2020-08-22 07:35:53 +0530

    Web Development codes added

---
+++ Web Development/test.js
@@ -0,0 +1,70 @@
+// const http = require('http');
+// const hostname = 'localhost';
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+// const bodyparser = require("body-parser");
+
+// https://mongoosejs.com/docs/
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const port = 5000;
+
+// Throwback connection success
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+// Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String
+  });
+
+// Prepare model
+const Contact = mongoose.model('Contact', contactSchema);
+
+// app.use(express.static('public', options)); //serving & intoducing static folder to express
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'pug'); //setting pug as template (view) engine
+// app.set('views', path.join(__dirname, "views")); //joining vi/ews direc. for storing templates
+
+app.get('/', function (req, res) {
+    res.status(200).render('index')
+});
+
+app.get('/index.pug', function (req, res) {
+    res.status(200).render('index')
+});
+
+// app.get("/", (req, res)=>{
+//     res.send("This is my first express insatallation and publish")
+// });
+
+app.get('/contact', (req, res)=>{
+    // const content = "This is a new line"
+    // const newline = {"entry":content}
+    // res.status(200).render('index', newline)
+    res.status(200).render('contact')
+});
+
+app.get('/projects', (req, res)=>{
+    res.status(200).render('projects')
+});
+
+
+app.post('/contact.pug', (req, res)=>{
+    var myData = new Contact(req.body)
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(404).send("This item has NOT been saved to the database")
+    })
+});
+
+// Start the nodejs server
+app.listen(port, /*hostname,*/ ()=>{
+    console.log(`Hosting started at $(port)`)
+});
